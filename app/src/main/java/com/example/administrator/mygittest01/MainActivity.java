de0b1e4eecd1c4976ac04016d36a5c3a37825bc2
吴江 <619944652@qq.com>
2016-06-27 17:57:51 +0800

    第三次修改提交

--- app/src/main/java/com/example/administrator/mygittest01/MainActivity.java
+++ app/src/main/java/com/example/administrator/mygittest01/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity{
     private Button loader;
     @Override
