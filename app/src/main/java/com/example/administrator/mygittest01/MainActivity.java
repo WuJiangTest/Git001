99b956b6445440155e70b6516fccc6be35b01d24
吴江 <619944652@qq.com>
2016-06-27 17:57:01 +0800

    第三次修改提交

--- app/src/main/java/com/example/administrator/mygittest01/MainActivity.java
+++ app/src/main/java/com/example/administrator/mygittest01/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.administrator.mygittest01;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity{
+    private Button loader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loader = (Button) this.findViewById(R.id.myButton);
+        loader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"按钮被点击了",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
